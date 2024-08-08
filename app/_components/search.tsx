b8d05293e1c915a6b2fda41309c24f06198f0d80
Felipe Rogerio <felipe.rogerio@outlook.com>
2024-08-08 17:35:32 -0300

    feat: ensure barbershops can be searched by title and services

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -5,12 +5,13 @@ import { Button } from "./ui/button"
 import { Input } from "./ui/input"
 import { useRouter } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+
 import { z } from "zod"
+import { useForm } from "react-hook-form"
 import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form"
 
 const formSchema = z.object({
-  search: z.string().trim().min(1, {
+  title: z.string().trim().min(1, {
     message: "Digite algo para buscar",
   }),
 })
@@ -19,14 +20,13 @@ const Search = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      search: "",
+      title: "",
     },
   })
-
   const router = useRouter()
 
   const handleSubmit = (data: z.infer<typeof formSchema>) => {
-    router.push(`/barbershops?search=${data.search}`)
+    router.push(`/barbershops?title=${data.title}`)
   }
 
   return (
@@ -34,7 +34,7 @@ const Search = () => {
       <form onSubmit={form.handleSubmit(handleSubmit)} className="flex gap-2">
         <FormField
           control={form.control}
-          name="search"
+          name="title"
           render={({ field }) => (
             <FormItem className="w-full">
               <FormControl>
@@ -44,7 +44,6 @@ const Search = () => {
                   className="w-full"
                 />
               </FormControl>
-
               <FormMessage />
             </FormItem>
           )}
