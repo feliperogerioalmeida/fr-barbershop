7f46f6f20c72cd328f7339c7c08dd612624b0c56
Felipe Rogerio <felipe.rogerio@outlook.com>
2024-08-08 16:27:50 -0300

    feat: add sign in with google

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "@/app/_lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth"
 import { Adapter } from "next-auth/adapters"
+import GoogleProvider from "next-auth/providers/google"
 
 const handler = NextAuth({
   adapter: PrismaAdapter(db) as Adapter,
