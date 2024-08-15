bc3b6cd1be0e3fd7218a5a20cac0c6ac16f16db4
Felipe Rogerio <felipe.rogerio@outlook.com>
2024-08-15 14:43:36 -0300

    feat: add BookingSummary component

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -31,7 +31,7 @@ const Bookings = async () => {
     },
   })
 
-  const concludeBookings = await db.booking.findMany({
+  const concludedBookings = await db.booking.findMany({
     where: {
       userId: (session?.user as any).id,
       date: {
@@ -55,7 +55,9 @@ const Bookings = async () => {
       <Header></Header>
       <div className="space-y-3 p-5">
         <h1 className="text-xl font-bold">Agendamentos</h1>
-
+        {confirmedBookings.length == 0 && concludedBookings.length == 0 && (
+          <p className="text-gray-400">Você não tem Agendamentos.</p>
+        )}
         {confirmedBookings.length > 0 && (
           <>
             <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
@@ -70,12 +72,12 @@ const Bookings = async () => {
           </>
         )}
 
-        {concludeBookings.length > 0 && (
+        {concludedBookings.length > 0 && (
           <>
             <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
               Finalizados
             </h2>
-            {concludeBookings.map((booking) => (
+            {concludedBookings.map((booking) => (
               <BookingItem
                 key={booking.id}
                 booking={JSON.parse(JSON.stringify(booking))}
