dac08938c279fe85f1891621494706e466054887
Felipe Rogerio <felipe.rogerio@outlook.com>
2024-08-20 09:58:06 -0300

    chore: fixed import for Dialog Content on sidebar-sheet

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -6,11 +6,10 @@ import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
 import Link from "next/link"
 import Image from "next/image"
-import { Dialog, DialogTrigger } from "./ui/dialog"
+import { Dialog, DialogTrigger, DialogContent } from "./ui/dialog"
 import { signOut, useSession } from "next-auth/react"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import SignInDialog from "./sign-in-dialog"
-import { DialogContent } from "@radix-ui/react-dialog"
 
 const SidebarSheet = () => {
   const { data } = useSession()
