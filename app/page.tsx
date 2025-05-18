fc8b1c179f90460cf4ea201414e8ed79b4e39782
Felipe Almeida <felipe.rogerio@outlook.com>
2025-05-17 22:04:15 -0300

    style: updated project name

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ const Home = async () => {
         {/* IMAGEM  */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
-            alt="Agende nos melhores com FSW Barber"
+            alt="Agende nos melhores com FR Barbershop"
             src="/banner-01.png"
             fill
             className="rounded-xl object-cover"
